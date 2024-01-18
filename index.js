95ec373dd95c629ea22afaaa178060c7f26601ab
“c.b.olha” <c.b.olha@viseven.com>
2024-01-18 11:42:58 -0300

    created function to add new note

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+let notes = [];
 const form = document.querySelector("#form");
 const noteTitle = document.querySelector("#noteTitle");
+const noteText = document.querySelector("#noteText");
 const formButtons = document.querySelector("#formButtons");
 
 const handleFormClick = (e) => {
@@ -22,3 +24,22 @@ const closeForm = () => {
 document.body.addEventListener("click", (e) => {
   handleFormClick(e);
 });
+
+const addNote = (note) => {
+  const newNote = {
+    title: note.title,
+    text: note.text,
+    color: "white",
+    id: notes.length > 0 ? notes[notes.length - 1].id + 1 : 1,
+  };
+
+  notes.push(newNote);
+};
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  const title = noteTitle.value;
+  const text = noteText.value;
+  const hasNote = title || text;
+  hasNote && addNote({ title, text });
+};
