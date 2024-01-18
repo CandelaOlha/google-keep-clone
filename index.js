889f70ec03f84286e4bdce387c71fdf3c03cfbdd
“c.b.olha” <c.b.olha@viseven.com>
2024-01-18 12:08:13 -0300

    created function to display notes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const form = document.querySelector("#form");
 const noteTitle = document.querySelector("#noteTitle");
 const noteText = document.querySelector("#noteText");
 const formButtons = document.querySelector("#formButtons");
+const placeholder = document.querySelector("#placeholder");
+const notesContainer = document.querySelector("#notes");
 
 const handleFormClick = (e) => {
   const isFormClicked = form.contains(e.target);
@@ -19,11 +21,41 @@ const closeForm = () => {
   form.classList.remove("form-open");
   noteTitle.classList.add("hidden");
   formButtons.classList.add("hidden");
+
+  noteTitle.value = "";
+  noteText.value = "";
 };
 
 document.body.addEventListener("click", (e) => {
   handleFormClick(e);
 });
+
+const displayNotes = () => {
+  const hasNotes = notes.length > 0;
+  placeholder.style.display = hasNotes ? "none" : "flex";
+
+  const displayedNotes = notes.reduce((acc, curr) => {
+    return (
+      acc +
+      `
+    <div style="background: ${curr.color};" class="note">
+    <h2 class="${curr.title && "note-title"}">${curr.title}</h2>
+    <p class="note-text">${curr.text}</p>
+    <div class="toolbar-container">
+      <div class="toolbar">
+        <img class="toolbar-color" src="https://icon.now.sh/palette">
+        <img class="toolbar-delete" src="https://icon.now.sh/delete">
+      </div>
+    </div>
+  </div>
+    `
+    );
+  }, "");
+
+  notesContainer.innerHTML = displayedNotes;
+
+  closeForm();
+};
 
 const addNote = (note) => {
   const newNote = {
@@ -34,6 +66,8 @@ const addNote = (note) => {
   };
 
   notes.push(newNote);
+
+  displayNotes();
 };
 
 form.onsubmit = (e) => {
